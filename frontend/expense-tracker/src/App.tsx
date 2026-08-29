3ca15d37a7bf35e45beb19f57fc9dcdec2a3e09a
sofia.surro <jaymee.surro@angkas.com>
2026-08-29 17:44:19 +0800

    feat: add routing structure and authentication handling

--- frontend/expense-tracker/src/App.tsx
+++ frontend/expense-tracker/src/App.tsx
@@ -1,18 +1,34 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+import Login from './pages/Auth/Login'
+import Signup from './pages/Auth/Signup'
+import Home from './pages/Dashboard/Home'
+import Income from './pages/Dashboard/Income'
+import Expense from './pages/Dashboard/Expense'
 
 const App = () => {
   return (
     <div className="">
       <Router>
-        
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/income" element={<Income />} />
+          <Route path="/expense" element={<Expense />} />
+        </Routes>
       </Router>
     </div>
   )
 }
 
-export default App
+export default App;
+
+const Root = () => {
+  //Checks if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  //Redirect to dashboard if authenticate, otherwise to login
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+};
