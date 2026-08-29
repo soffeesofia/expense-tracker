642cb1f42546cf192e0d0259f108ed4a07216f6d
sofia.surro <jaymee.surro@angkas.com>
2026-08-29 17:23:00 +0800

    build: setup frontend dependencies

---
+++ frontend/expense-tracker/src/App.tsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className="">
+      <Router>
+        
+      </Router>
+    </div>
+  )
+}
+
+export default App
